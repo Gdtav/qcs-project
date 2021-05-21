2eddd780ae750d1aac8d1a17c14ac028641da07d
Pedro Mendonça <pedro.mendonca1@hotmail.com>
2021-05-21 16:02:24 +0100

    Added incomplete example of mocks

--- src/test/java/HandlerTest.java
+++ src/test/java/HandlerTest.java
@@ -1,10 +1,17 @@
+import com.sun.net.httpserver.HttpExchange;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
 
 class HandlerTest {
 
+    @Mock
+    HttpExchange httpExchange;
+
     @Test
-    void handle() {
+    void handle(HttpExchange exchange) {
+        when(exchange.getRequestURI()).thenReturn();
     }
 }
