1cf039d7083a40aff5062a90e463c0b15e43b88e
Pedro Mendonça <pedro.mendonca1@hotmail.com>
2021-05-30 11:24:19 +0100

    Added tests to RequestTest

--- src/test/java/RequestTest.java
+++ src/test/java/RequestTest.java
@@ -1,14 +1,45 @@
+import com.sun.net.httpserver.HttpExchange;
+import org.junit.Before;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.mockito.Spy;
+import org.mockito.runners.MockitoJUnitRunner;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.LinkedList;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.*;
 
 class RequestTest {
 
-    @Test
-    void getBody() {
+    @Mock
+    private HttpExchange exchange;
+
+    @Before public void initMocks() {
+        MockitoAnnotations.initMocks(this);
     }
 
+    @Test
+    public void getBodyWithBodyFetched() {
+        doReturn("GET").when(exchange).getRequestMethod();
+
+        Request request = new Request(exchange);
+
+        request.body = new LinkedList<>();
+        request.bodyFetched = true;
+
+        Assertions.assertEquals(request.getBody(), new LinkedList<>());
+    }
+
+    @Test
+    void getBodyWithNoData() {
+    }
+
+    @Test
+    void getBodyWithData() {
+    }
 }
